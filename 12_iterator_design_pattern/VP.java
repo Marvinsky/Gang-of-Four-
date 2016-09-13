4a0196aa925a02f240d5d3e40224c34b43b75e17
marvin <nivramsky@gmail.com>
2016-09-13 16:57:09 -0500

    partial commit for Iterator design pattern

---
+++ 12_iterator_design_pattern/VP.java
@@ -0,0 +1,18 @@
+
+public class VP {
+	private String name;
+	private String division;
+
+	public VP(String n, String d) {
+		name = n;
+		division = d;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void print() {
+		System.out.println("Name: " + name + " Division: "+ division);
+	}
+}
